86fd7c8c9a14b0becea89befefb7e3992be5603c
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-05-06 07:57:05 +0300

    add second row of weight and age in home page

--- lib/home.dart
+++ lib/home.dart
@@ -10,59 +10,140 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   bool ismale = true;
   int age = 20;
   double heightval = 170;
-  double weight = 70;
+  int weight = 70;
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       backgroundColor: Theme.of(context).canvasColor,
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        title: Text("Body Mass Index",style: TextStyle(color: Colors.white),),
+        title: Text(
+          "Body Mass Index",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
-      ) ,
+      ),
       body: Padding(
-       padding: const EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-          Expanded(
-            child: Row( children: [
-              m1expanded(context,"male"),
-              SizedBox(width: 15,),
-              m1expanded(context,"female"),
-            ],),
-          )
-        ],),
+            Expanded(
+              child: Row(
+                children: [
+                  m1expanded(context, "male"),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  m1expanded(context, "female"),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  m2expanded(context, "weight"),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  m2expanded(context, "age"),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Expanded m1expanded(BuildContext context ,String Type ) {
+  Expanded m1expanded(BuildContext context, String Type) {
     return Expanded(
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  ismale = Type=="male" ?true : false;
-                });
-              },
-              child: Container( 
-                decoration: BoxDecoration(
-                     color: (Type=="male" && ismale == true) || (Type=="female" && ismale== false) ? Colors.teal : Colors.blueGrey,
-                     borderRadius: BorderRadius.circular(8)
-                ),
-                padding: EdgeInsets.symmetric(vertical: 15), 
-                margin: EdgeInsets.symmetric(vertical: 15),  
-                child: Column(
-                children: [
-                  Icon(Type=="male" ? Icons.male : Icons.female,size: 70,color: Colors.white,),
-                  SizedBox(height: 8,),
-                  Text(Type=="male" ?"Male" : "Female",style: Theme.of(context).textTheme.labelMedium,)
-                ],
-              ),),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            ismale = Type == "male" ? true : false;
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              color: (Type == "male" && ismale == true) ||
+                      (Type == "female" && ismale == false)
+                  ? Colors.teal
+                  : Colors.blueGrey,
+              borderRadius: BorderRadius.circular(8)),
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          margin: const EdgeInsets.symmetric(vertical: 15),
+          child: Column(
+            children: [
+              Icon(
+                Type == "male" ? Icons.male : Icons.female,
+                size: 70,
+                color: Colors.white,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                Type == "male" ? "Male" : "Female",
+                style: Theme.of(context).textTheme.labelMedium,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded m2expanded(BuildContext context, String Type) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.blueGrey, borderRadius: BorderRadius.circular(8)),
+        padding: const EdgeInsets.symmetric(vertical: 15),
+        margin: const EdgeInsets.only(bottom: 15),
+        child: Column(
+          children: [
+            Text(
+              Type == "weight" ? "Weight" : "Age",
+              style: Theme.of(context).textTheme.labelMedium,
             ),
-          );
+            Text(Type == "weight" ? weight.toString() : age.toString(),
+                style: Theme.of(context).textTheme.headlineLarge),
+           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               FloatingActionButton.small(
+                backgroundColor: Colors.teal,
+                onPressed: (){
+              setState(() {
+                Type == "weight" ? weight -- : age--;
+              });
+            },
+            heroTag: Type == "weight" ? "weight--" : "age--",
+            child: Icon(Icons.remove,color: Colors.white,),
+            )  ,
+            SizedBox(width: 10,),
+            FloatingActionButton.small(
+              backgroundColor: Colors.teal,
+              onPressed: (){
+              setState(() {
+                Type == "weight" ? weight ++ : age++;
+              });
+            },
+            heroTag: Type == "weight" ? "weight++" : "age++",
+            child: Icon(Icons.add,color: Colors.white,),
+            )    
+            ],
+           )
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+ 
--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,29 @@ class Bmi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData( 
-        primaryColor: Colors.teal,
-        canvasColor: Colors.black,
-        textTheme: const TextTheme( 
-            labelLarge:TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold,),
-            labelMedium: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.bold,)
-        )
-      ),
-      home:  HomePage(),
+      theme: ThemeData(
+          primaryColor: Colors.teal,
+          canvasColor: Colors.black,
+          textTheme: const TextTheme(
+            labelLarge: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+            labelMedium: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+            headlineLarge: TextStyle(
+              color: Colors.white,
+              fontSize: 42,
+              fontWeight: FontWeight.w900,
+            ),
+          )),
+      home: HomePage(),
       // home: ResultPage(age: 26,ismale: true,result: 18.4,),
     );
   }
